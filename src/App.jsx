4ee3f6fabb1c58c2c29fd9d062527791a8baf092
NoothangiBharathi <noothangi.bharathi1999@gmail.com>
2024-11-01 20:25:59 +0530

    learning react js

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import React from 'react'
+import './App.css'
+
+let a = "Mangoes";
+let b = "Apples"
+
+
+ function App() {
+  return (
+    <div className='container'>
+      <h1>This is paragraph</h1>
+    </div>
+  )
+}
+
+export default App
